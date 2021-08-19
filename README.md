a353d920522158ca5e6d01a57cb3eeb02547f5ad
Kazuki Kaneoka <kzkaneoka@gmail.com>
2021-08-19 15:05:34 +0900

    solved perl5/raindrops/

--- README.md
+++ README.md
@@ -13,3 +13,4 @@ Learn programming languages by [exercism](https://exercism.io/).
 - [Bob](./perl5/bob/)
 - [Leap](./perl5/leap/)
 - [Grains](./perl5/grains/)
+- [Raindrops](./perl5/raindrops/)
---
+++ perl5/raindrops/Raindrops.pm
@@ -0,0 +1,24 @@
+package Raindrops;
+use strict;
+use warnings;
+use Exporter 'import';
+our @EXPORT_OK = qw(raindrop);
+
+sub raindrop {
+  my ($number) = @_;
+  my $string = "";
+
+  if ($number % 3 == 0) {
+    $string .= "Pling";
+  }
+  if ($number % 5 == 0) {
+    $string .= "Plang";
+  }
+  if ($number % 7 == 0) {
+    $string .= "Plong";
+  }
+
+  return $string || $number;
+}
+
+1;
