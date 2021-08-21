726f47441cf0826b28084c48d293e8228d2ca6e5
Kazuki Kaneoka <kzkaneoka@gmail.com>
2021-08-21 12:53:37 +0900

    formatted perl5/raindrops by perltidy

--- perl5/raindrops/Raindrops.pm
+++ perl5/raindrops/Raindrops.pm
@@ -5,20 +5,20 @@ use Exporter 'import';
 our @EXPORT_OK = qw(raindrop);
 
 sub raindrop {
-  my ($number) = @_;
-  my $string = "";
+    my ($number) = @_;
+    my $string = "";
 
-  if ($number % 3 == 0) {
-    $string .= "Pling";
-  }
-  if ($number % 5 == 0) {
-    $string .= "Plang";
-  }
-  if ($number % 7 == 0) {
-    $string .= "Plong";
-  }
+    if ( $number % 3 == 0 ) {
+        $string .= "Pling";
+    }
+    if ( $number % 5 == 0 ) {
+        $string .= "Plang";
+    }
+    if ( $number % 7 == 0 ) {
+        $string .= "Plong";
+    }
 
-  return $string || $number;
+    return $string || $number;
 }
 
 1;
