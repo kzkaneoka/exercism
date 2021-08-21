3cc1443d1d10699281073bd7a88d82716ff701f0
Kazuki Kaneoka <kzkaneoka@gmail.com>
2021-08-21 13:28:57 +0900

    fixed bugs in perl5/scrabble-score

--- perl5/scrabble-score/ScrabbleScore.pm
+++ perl5/scrabble-score/ScrabbleScore.pm
@@ -5,18 +5,20 @@ use Exporter 'import';
 our @EXPORT_OK = qw(score);
 
 my %CHAR_POINT = (
-  ( map { $_ => 1 } qw( A E I O U L N R S T ) ),
-  ( map { $_ => 2 } qw( D G ) ),
-  ( map { $_ => 3 } qw( B C M P ) ),
-  ( map { $_ => 4 } qw( F H V W Y ) ),
-  ( map { $_ => 5 } qw( K ) ),
-  ( map { $_ => 8 } qw( J X ) ),
-  ( map { $_ => 10 } qw( Q Z ) ),
+    ( map { $_ => 1 } qw( A E I O U L N R S T ) ),
+    ( map { $_ => 2 } qw( D G ) ),
+    ( map { $_ => 3 } qw( B C M P ) ),
+    ( map { $_ => 4 } qw( F H V W Y ) ),
+    ( map { $_ => 5 } qw( K ) ),
+    ( map { $_ => 8 } qw( J X ) ),
+    ( map { $_ => 10 } qw( Q Z ) ),
 );
 
 sub score {
-  my ( $word, %extensions ) = @_;
-  return sum( 0, @CHAR_POINT{ split //, uc $word } );
+    my ( $word, %extensions ) = @_;
+    my $total_score = 0;
+    $total_score += $CHAR_POINT{$_} for ( split( //, uc $word ) );
+    return $total_score;
 }
 
 1;
